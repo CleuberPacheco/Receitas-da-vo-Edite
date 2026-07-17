6c53031d98e7aa16bd00925496a98d8bea987b03
CleuberPacheco <profcleuberjr@gmail.com>
2026-07-16 23:06:14 -0300

    Rename 'fotoUrl' to 'urlFoto' in ReceitasMocks

--- Front-End/src/mocks/ReceitasMocks.ts
+++ Front-End/src/mocks/ReceitasMocks.ts
@@ -8,7 +8,7 @@ export const receitasMock: Receita[] = [
         nome: 'Bolo de Cenoura com Brigadeiro ',
         categoria: 'doces',
         subcategoria: 'Bolos',
-        fotoUrl: 'https://images.unsplash.com/photo-1621466560271-4b055938885b?q=80&w=300', // Foto fofa real
+        urlFoto: 'https://images.unsplash.com/photo-1621466560271-4b055938885b?q=80&w=300', // Foto fofa real
         ingredientes: ['Cenoura', 'Ovos', 'Farinha'],
         modoPreparo: 'Misture e asse.'
     },
@@ -25,7 +25,7 @@ export const receitasMock: Receita[] = [
         nome: 'Bolo de Cenoura com Brigadeiro',
         categoria: 'doces',
         subcategoria: 'Bolos',
-        fotoUrl: 'https://images.unsplash.com/photo-1621466560271-4b055938885b?q=80&w=300', // Foto fofa real
+        urlFoto: 'https://images.unsplash.com/photo-1621466560271-4b055938885b?q=80&w=300', // Foto fofa real
         ingredientes: ['Cenoura', 'Ovos', 'Farinha'],
         modoPreparo: 'Misture e asse.'
     },
@@ -42,7 +42,7 @@ export const receitasMock: Receita[] = [
         nome: 'Bolo de Cenoura com Brigadeiro',
         categoria: 'doces',
         subcategoria: 'Bolos',
-        fotoUrl: 'https://images.unsplash.com/photo-1621466560271-4b055938885b?q=80&w=300', // Foto fofa real
+        urlFoto: 'https://images.unsplash.com/photo-1621466560271-4b055938885b?q=80&w=300', // Foto fofa real
         ingredientes: ['Cenoura', 'Ovos', 'Farinha'],
         modoPreparo: 'Misture e asse.'
     },
@@ -59,7 +59,7 @@ export const receitasMock: Receita[] = [
         nome: 'Bolo de Cenoura com Brigadeiro',
         categoria: 'doces',
         subcategoria: 'Bolos',
-        fotoUrl: 'https://images.unsplash.com/photo-1621466560271-4b055938885b?q=80&w=300', // Foto fofa real
+        urlFoto: 'https://images.unsplash.com/photo-1621466560271-4b055938885b?q=80&w=300', // Foto fofa real
         ingredientes: ['Cenoura', 'Ovos', 'Farinha'],
         modoPreparo: 'Misture e asse.'
     },
@@ -78,7 +78,7 @@ export const receitasMock: Receita[] = [
         nome: 'Torta de Frango Cremosa',
         categoria: 'salgados',
         subcategoria: 'Tortas Salgadas',
-        fotoUrl: 'https://images.unsplash.com/photo-1544025162-d76694265947?q=80&w=300', // Foto real
+        urlFoto: 'https://images.unsplash.com/photo-1544025162-d76694265947?q=80&w=300', // Foto real
         ingredientes: ['Frango', 'Creme de Leite', 'Massa'],
         modoPreparo: 'Asse até dourar. era uma vez um lugarzinho no meio do mato onde morava uma velhinha chamada Dona Edite. Ela era conhecida por suas receitas deliciosas, que ela preparava com muito amor e carinho. Um dia, Dona Edite decidiu compartilhar suas receitas com o mundo, criando um caderno de receitas especial. Ela começou a escrever suas receitas favoritas, desde bolos e tortas até pratos salgados e sobremesas. Cada receita era acompanhada de uma história única, que contava como ela aprendeu a fazer aquele prato ou como ele se tornou especial para ela. O caderno de receitas de Dona Edite se tornou um tesouro para todos que o tinham, e suas receitas continuaram a ser passadas de geração em geração, mantendo viva a memória e o sabor da cozinha da Dona Edite.'
     },
@@ -87,7 +87,7 @@ export const receitasMock: Receita[] = [
         nome: 'Pão de Queijo Mineiro',
         categoria: 'salgados',
         subcategoria: 'Lanches',
-        fotoUrl: 'https://images.unsplash.com/photo-1627311124401-44755d4710bd?q=80&w=300', // Foto real
+        urlFoto: 'https://images.unsplash.com/photo-1627311124401-44755d4710bd?q=80&w=300', // Foto real
         ingredientes: ['Polvilho', 'Queijo', 'Ovos'],
         modoPreparo: 'Forme bolinhas e asse.'
     } 
